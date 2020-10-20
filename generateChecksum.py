ab0a853327a9f94c0165c05c1e2747cf5ab1d79e
Titus Nguyen <titusstar007@gmail.com>
2020-10-19 17:19:45 -0700

    Added more definitions and formatted csv files without quotechar
    Simplified code for more definitions, csv_writer, format_csv. Added try/except for more errors. Removed extra blank lines from csv, with the cost of removing quotechar

--- generateChecksum.py
+++ generateChecksum.py
@@ -10,10 +10,27 @@ def gen_sha1(path, abspath=True):
         sha1.update(buffer)
         if abspath is True:
             print(f'{os.path.abspath(path)} : {sha1.hexdigest()}')
-            return os.path.abspath(path), sha1.hexdigest()
+            return [os.path.abspath(path), sha1.hexdigest()]
         else:
             print(f'{path} : {sha1.hexdigest()}')
-            return path, sha1.hexdigest()
+            return [path, sha1.hexdigest()]
+
+
+def csv_writer(material, csv_filename):
+    with open(f'{csv_filename}.csv', 'a') as csvFile:
+        csvWriter = csv.writer(csvFile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvWriter.writerow(material)
+
+
+def format_csv(csv_file_path):
+    with open(csv_file_path, 'r') as file:
+        csvList = file.readlines()
+        for index, line in enumerate(csvList):
+            if line == '\n':
+                del csvList[index]
+        with open(csv_file_path, 'w') as newFile:
+            for key in csvList:
+                newFile.write(key)
 
 
 class dir_sum:
@@ -34,37 +51,39 @@ class dir_sum:
                         if line.startswith('#') or line == "\n":
                             del ignoreList[index]
                         ignoreList[index] = line.rstrip("\n")
-                print(ignoreList)
             except FileNotFoundError:
                 print(".gitignore Not Found")
-
+                ignoreList = None
+                
         if csv_write:
             with open(f'{csv_filename}.csv', 'w+'):
                 pass
 
-        for item in os.listdir(self.dirPath):
-            hashCheckCondition = False
-            if self.ignoreFile:
-                for ignoreItem in ignoreList:
-                    if ignoreItem == item:
-                        hashCheckCondition = True
+            for item in os.listdir(self.dirPath):
+                hashCheckCondition = False
+                if self.ignoreFile:
+                    try:
+                        for ignoreItem in ignoreList:
+                            if ignoreItem == item:
+                                hashCheckCondition = True
+                    except TypeError:
+                        pass
 
-            if hashCheckCondition:
-                continue
-            if not os.path.isdir(item):
-                if abspath:
-                    filePath = os.path.abspath(os.path.join(self.dirPath, item))
-                else:
-                    filePath = item
-                try:
-                    output = gen_sha1(os.path.join(self.dirPath, item), abspath)
-                    if csv_write:
-                        with open(f'{csv_filename}.csv', 'a') as csvFile:
-                            csvWriter = csv.writer(csvFile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                            csvWriter.writerow(output)
-                except PermissionError as e:
-                    print(f"Error; Could not hash {filePath} : Permission Error")
-                    print(e)
+                if hashCheckCondition:
+                    continue
+                if not os.path.isdir(item):
+                    if abspath:
+                        filePath = os.path.abspath(os.path.join(self.dirPath, item))
+                    else:
+                        filePath = item
+                    try:
+                        output = gen_sha1(os.path.join(self.dirPath, item), abspath)
+                        if csv_write:
+                            csv_writer(output, csv_filename)
+                    except PermissionError as e:
+                        print(f"Error; Could not hash {filePath} : Permission Error")
+                        print(e)
+        format_csv(f'{csv_filename}.csv')
 
 
     def recursive_checksum_dir(self, csv_write, abspath, csv_filename):
@@ -81,15 +100,13 @@ class dir_sum:
                 try:
                     output = gen_sha1(os.path.join(root, file), abspath)
                     if csv_write:
-                        with open(f'{csv_filename}.csv', 'a') as csvFile:
-                            csvWriter = csv.writer(csvFile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                            csvWriter.writerow(output)
-
+                        csv_writer(output, csv_filename)
                 except PermissionError as e:
                     print(f"Error; Could not hash {filePath} : Permission Error")
                     print(e)
+        format_csv(f'{csv_filename}.csv')
 
 
 if __name__ == '__main__':
-    Tracer = dir_sum('D:\\PycharmProjects\\Tracer')
-    Tracer.checksum_dir(True, True, 'Tracer')
+    Tracer = dir_sum('D:\\PycharmProjects')
+    Tracer.recursive_checksum_dir(True, True, 'Tracer')
